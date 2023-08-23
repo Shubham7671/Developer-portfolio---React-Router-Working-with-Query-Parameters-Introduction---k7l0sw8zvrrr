46f775ba1a48fe6d73539acb1f3d1b720e9a889f
Shubham7671 <sk767196@gmail.com>
2023-08-23 14:16:09 +0530

    done

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,8 @@
+'use client'
 import React from "react";
+import { useRouter } from "next/navigation";
 const Blog = () => {
-
+let navigate=useRouter();
 
     return (
         <div id="blog-page">
@@ -9,8 +11,8 @@ const Blog = () => {
             <h2>Checkout my awesome blogs </h2>
             <h3><a href='https://www.newtonschool.co/post/top-13-web-developer-interview-questions'>Web Development Interview questions</a></h3>
             <h3><a href='https://www.newtonschool.co/post/graph-data-structure-explained-with-examples'>Graph Data Structures</a></h3>
-            <button id="blog-to-info"> Go to Info</button>
-            <button id="blog-to-home"> Go to Home</button>
+            <button id="blog-to-info" onClick={()=>navigate.push('/info')}> Go to Info</button>
+            <button id="blog-to-home" onClick={()=>navigate.push('/')}> Go to Home</button>
         </div>
     )
 }
--- app/info/page.js
+++ app/info/page.js
@@ -1,7 +1,10 @@
+'use client'
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const Info = () => {
 
+const Info = () => {
+let navigate=useRouter();
 
     return (
         <div id="info-page">
@@ -10,8 +13,8 @@ const Info = () => {
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
-             <button id="info-to-blog"> Go to Blog</button>
-            <button id="info-to-home"> Go to Home</button>
+             <button id="info-to-blog"onClick={()=>navigate.push('/blog')}> Go to Blog</button>
+             <button id="info-to-home"onClick={()=>navigate.push('/')}> Go to Home</button>
         </div>
     )
 }
